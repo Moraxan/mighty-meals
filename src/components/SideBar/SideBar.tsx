2f9f113a1927ac7666a863140bf9eeeddd97c3b0
Chab93 <vikcrille93@gmail.com>
2023-04-05 18:51:41 +0200

    updated close button logo

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import SideBarSection from '../SideBarSection/SideBarSection';
+import closeLogo from "../../images/close.png"
 import './SideBar.css'
 
 //@ts-ignore
@@ -19,7 +20,9 @@ export default function SideBar() {
       <Offcanvas id="filter-canvas" show={show} onHide={handleClose} placement="end">
         <Offcanvas.Header id="filter-header" className="d-flex align-items-start">
           <Offcanvas.Title id="filter-title" >Search & Filter</Offcanvas.Title>
-          <Button className="filter-close-btn" onClick={handleClose}>X</Button>
+          <Button className="filter-close-btn" onClick={handleClose}>
+            <img className="filter-close-logo" src={closeLogo} alt="close"/>
+          </Button>
         </Offcanvas.Header>
         <Offcanvas.Body className="filter-body">
           <SideBarSection />
