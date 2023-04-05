80b97aaedb3f2dacfd31d54a1d2819d8e1201e96
Moraxan <83705522+Moraxan@users.noreply.github.com>
2023-04-05 12:10:40 +0200

    Searching for ingredients now works as intended

--- src/components/SearchIngredients.tsx
+++ src/components/SearchIngredients.tsx
@@ -7,7 +7,7 @@ const SearchIngredients = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [suggestions, setSuggestions] = useState<string[]>([]);
 
-//This fetches the .csv. Super wierd that I don't have to parse it this time.
+//This fetches the .csv. It reads the CSV and the response.text method parses it to text strings. The data.split breaks every row and tosses it into the setIngredientArray.
   useEffect(() => {
     fetch('./src/components/commonIngredients.csv')
       .then(response => response.text())
@@ -28,22 +28,22 @@ const SearchIngredients = () => {
     setSuggestions(emptyArray);
   };
 //This does what it says. It takes care of the items selected from the <ul> and stores it in the ingredientChoices array. const [ingredientChoices, setIngredientChoices] = useState<string[]>([]);
-  const handleSelectFromList = (element: HTMLElement) => {
-    const selectUserData = element.textContent?.toLowerCase();
-    const updatedChoices = selectUserData && ingredientChoices.includes(selectUserData)
-      ? ingredientChoices.filter(choice => choice !== selectUserData)
-      : [...ingredientChoices, selectUserData];
-    setIngredientChoices(updatedChoices);
-    setSearchTerm('');
-    setSuggestions([]);
-  };
+function handleSelectFromList(element: HTMLElement) {
+  const selectUserData = element.textContent?.toLowerCase() ?? '';
+  const updatedChoices = selectUserData && ingredientChoices.includes(selectUserData)
+    ? ingredientChoices.filter(choice => choice !== selectUserData)
+    : [...ingredientChoices, selectUserData];
+  setIngredientChoices(updatedChoices);
+  setSearchTerm('');
+  setSuggestions([]);
+}
 //This clears the ingredients array
   const handleClearIngredients = () => {
     setIngredientChoices([]);
     setSearchTerm('');
     setSuggestions([]);
   };
-
+//This is my array of all the selected ingredients.
   const selectionFilter = (choices: string[]) => choices.join(', ');
 //autocom-box is styled with the the local SearchIngredient.css
   return (
