c46edf65eef8fd89807beeb2de05e5682584373e
Moraxan <83705522+Moraxan@users.noreply.github.com>
2023-04-06 14:42:33 +0200

    Lifted out a fetch button

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SearchIngredients from './components/SearchingredientsBar/SearchIngredients'
-import IngredientSearch from './components/SearchingredientsBar/IngredientSearch'
+import SearchIngredients from './components/searchingredientsbar/SearchIngredients'
+import IngredientSearch from './components/searchingredientsbar/IngredientSearch'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <SearchIngredients />
-      <IngredientSearch />
+      
     </div>
   )
 }
---
+++ src/components/fetchbutton/fetchbutton.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import SearchIngredients from '../searchingredientsbar/SearchIngredients';
+
+function FetchButton({ ingredientChoices, setRecipes }) {
+  const [apiKey, setApiKey] = useState(localStorage.getItem('apiKey') || '');
+  const tvmhHits = 4;
+  const apiString = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${encodeURIComponent(
+    ingredientChoices.join(",")
+  )}&ranking=2&ignorePantry=true&number=${tvmhHits}`;
+
+  console.log(encodeURI(apiString));
+
+  //Lets us store our own API-keys for debugging puropses.
+  const handleApiKeyChange = (event) => {
+    setApiKey(event.target.value);
+    localStorage.setItem('apiKey', event.target.value);
+  };
+
+  //Get's the generated API-string and fetches it from the API.
+  const handleClick = () => {
+    fetch(apiString)
+      .then((response) => response.json())
+      .then((data) => setRecipes(data))
+      .catch(() => alert("Cannot connect, check your API key."));
+  };
+
+  //Ths html for the component. The API-key input could be deleted later on or handled by another component.
+  return (
+    <div>
+      <label htmlFor="apiKey">API Key:</label>
+      <input type="text" id="apiKey" value={apiKey} onChange={handleApiKeyChange} />
+      <button onClick={handleClick}>GO!</button>
+    </div>
+  );
+}
+
+export default FetchButton;
