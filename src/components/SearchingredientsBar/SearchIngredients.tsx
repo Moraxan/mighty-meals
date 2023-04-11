5ea25c562365f9da8fb4f4b6b7ab9257cb72b962
Moraxan <83705522+Moraxan@users.noreply.github.com>
2023-04-11 18:55:32 +0200

    Rewritten functions to const

--- src/components/SearchingredientsBar/SearchIngredients.tsx
+++ src/components/SearchingredientsBar/SearchIngredients.tsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './SearchIngredients.css';
 
-// Define the SearchIngredients function. Also gives it the setIngredientChoices prop. That can then be used by other components, as the fetch button.
-export function SearchIngredients({ setIngredientChoices }) {
-  // Declare state variables using the useState hook
-  const [ingredientArray, setIngredientArray] = useState<string[]>([]);
-  const [searchTerm, setSearchTerm] = useState<string>('');
-  const [suggestions, setSuggestions] = useState<string[]>([]);
+export const SearchIngredients = ({ setIngredientChoices }) => {
+  const [ingredientArray, setIngredientArray] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
 
-  // Use the useEffect hook to fetch the CSV file and update the ingredientArray state variable
   useEffect(() => {
     fetch('./src/components/searchingredientsbar/commonIngredients.csv')
       .then(response => response.text())
@@ -18,8 +15,7 @@ export function SearchIngredients({ setIngredientChoices }) {
       });
   }, []);
 
-  // Define the handleInputChange function to handle changes in the search box
-  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
+  const handleInputChange = (event) => {
     const userData = event.target.value;
     setSearchTerm(userData);
 
@@ -28,51 +24,45 @@ export function SearchIngredients({ setIngredientChoices }) {
       : [];
 
     setSuggestions(emptyArray);
-  }
+  };
 
-  // Define the handleSelectFromList function to handle selecting items from the suggestion list
-  function handleSelectFromList(element: HTMLElement) {
-    const selectUserData = element.textContent?.toLowerCase() ?? '';
-    const updatedChoices = selectUserData && setIngredientChoices((choices: string[]) =>
+  const handleSelectFromList = (event) => {
+    const selectUserData = event.currentTarget.textContent?.toLowerCase() ?? '';
+    const updatedChoices = selectUserData && setIngredientChoices((choices) =>
       choices.includes(selectUserData)
         ? choices.filter(choice => choice !== selectUserData)
         : [...choices, selectUserData]
     );
     setSearchTerm('');
     setSuggestions([]);
-  }
+  };
 
-  // Define the handleClearIngredients function to clear the ingredientChoices state variable
-  function handleClearIngredients() {
+  const handleClearIngredients = () => {
     setIngredientChoices([]);
     setSearchTerm('');
     setSuggestions([]);
-  }
-  
-  // Define the selectionFilter function to convert the ingredientChoices array to a string with comma-separated values
-  function selectionFilter(choices: string[]): string {
+  };
+
+  const selectionFilter = (choices) => {
     if (Array.isArray(choices)) {
       return choices.join(', ');
     } else {
       return '';
     }
   }
-  
-  
-  // Render the component
+
   return (
     <div className="search-input">
       <input type="text" value={searchTerm} onChange={handleInputChange} />
       <ul className="autocom-box">
-        {/* suggestion.slice(0,10) makes the list only display a maximum of 10 results. */}
         {suggestions.slice(0, 10).map((data, index) => (
-          <li key={index} onClick={(event) => handleSelectFromList(event.currentTarget)}>
+          <li key={index} onClick={handleSelectFromList}>
             {data.toLowerCase()}
           </li>
         ))}
       </ul>
       <button className="clear-ingredients" onClick={handleClearIngredients}>Clear</button>
-      <p id="selectedIngredients">{selectionFilter(setIngredientChoices)}</p>
+      <p className="selections">{selectionFilter(setIngredientChoices)}</p>
     </div>
   );
-}
+};
--- src/components/fetchbutton/fetchbutton.tsx
+++ src/components/fetchbutton/fetchbutton.tsx
@@ -1,20 +1,17 @@
 import { useState } from 'react';
 
-//Gives the component 2 props which can be accessed.
-export function FetchButton({ ingredientChoices, setRecipes }) {
+export const FetchButton = ({ ingredientChoices, setRecipes }) => {
   const [apiKey, setApiKey] = useState(localStorage.getItem('apiKey') || '');
   const tvmhHits = 4;
   const apiString = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${encodeURIComponent(
     ingredientChoices.join(",")
   )}&ranking=2&ignorePantry=true&number=${tvmhHits}`;
 
-  //Lets us store our own API-keys for debugging puropses.
   const handleApiKeyChange = (event) => {
     setApiKey(event.target.value);
     localStorage.setItem('apiKey', event.target.value);
   };
 
-  //Get's the generated API-string and fetches it from the API.
   const handleClick = () => {
     fetch(apiString)
       .then((response) => response.json())
@@ -23,7 +20,6 @@ export function FetchButton({ ingredientChoices, setRecipes }) {
       console.log(apiString)
   };
 
-  //Ths html for the component. The API-key input could be deleted later on or handled by another component.
   return (
     <div>
       <label htmlFor="apiKey">API Key:</label>
@@ -31,5 +27,4 @@ export function FetchButton({ ingredientChoices, setRecipes }) {
       <button onClick={handleClick}>GO!</button>
     </div>
   );
-}
-
+};
