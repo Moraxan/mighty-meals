0644d465f518e4c5ac91b7a46ad2769e47fd54a2
Carl-Johan Svensson <calletrumma@gmail.com>
2023-04-16 20:54:26 +0200

    Commit before merge to development

--- src/components/RecipePage/RecipePage.tsx
+++ src/components/RecipePage/RecipePage.tsx
@@ -2,16 +2,16 @@ import { useEffect, useState } from 'react';
 import './RecipePage.css';
 
 export const RecipePage = () => {
+  const [recipeId, setRecipeId] = useState(660697);
   const [recipeData, setRecipeData] = useState(null);
 
   useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem('recipeFetch'));
+    const storedData = JSON.parse(localStorage.getItem(`recipeFetch_${recipeId}`));
     if (storedData) {
       setRecipeData(storedData);
     } else {
-      const receivedId = 660697;
       const apiKey = '62351b97ce5e483ab975407ba6e4bdb9';
-      const url = `https://api.spoonacular.com/recipes/${receivedId}/information?apiKey=${apiKey}`;
+      const url = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`;
 
       fetch(url)
         .then((response) => {
@@ -22,34 +22,64 @@ export const RecipePage = () => {
           }
         })
         .then((recipeData) => {
-          localStorage.setItem('recipeFetch', JSON.stringify(recipeData));
+          localStorage.setItem(`recipeFetch_${recipeId}`, JSON.stringify(recipeData));
           setRecipeData(recipeData);
         })
         .catch((error) => {
           console.error(error);
         });
     }
-  }, []);
+  }, [recipeId]);
+
+  const handleSwitchRecipe = () => {
+    if (recipeId === 660697) {
+      setRecipeId(634091);
+    } else {
+      setRecipeId(660697);
+    }
+  };
 
   return (
-    <div className="ingredients-outer">
-      
-        <div className="ingredients-box">
-          <p>INGREDIENTS</p>
-        </div> 
-        <div className="ingredients-body">
-          <br />
-          {recipeData ? (
-            <ul>
-              {recipeData.extendedIngredients.map((ingredient) => (
-                <li key={ingredient.id}>{ingredient.original}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>Loading recipe data...</p>
-          )}
+    <>
+      <button onClick={handleSwitchRecipe}>Switch Recipe</button>
+      {recipeData && (
+        <div className="recipe-container">
+          <div className="dish-image-container">
+            <img src={recipeData.image} alt="Your dish" />
+          </div>
+          <div className="ingredients-directions-container">
+            <div className="ingredients-body">
+              <div className="ingredients-container">
+                <div className="ingredients-box">
+                  <p>INGREDIENTS</p>
+                </div>
+                <br />
+                <ul>
+                  {recipeData.extendedIngredients.map((ingredient) => (
+                    <li key={ingredient.id}>{ingredient.original}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="directions-container">
+              <div className="directions-box">
+                <p>DIRECTIONS</p>
+              </div>
+              <br />
+              <ul>
+                {recipeData.analyzedInstructions[0].steps.map((step) => (
+                  <li key={step.number}>
+                    <label>
+                      <input type="checkbox" />
+                      {step.step}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
-    </div>
-   
+      )}
+    </>
   );
-}
+};
