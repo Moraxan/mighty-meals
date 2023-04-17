008096d0474b3bcad76f7c8669611b143d531a54
Carl-Johan Svensson <calletrumma@gmail.com>
2023-04-17 09:31:08 +0200

    Revert "More fixes"
    This reverts commit c37ffbf0b7f3db5912bb8351e113885536d95a9a.

--- src/components/RecipePage/BackButton.tsx
+++
@@ -1,5 +0,0 @@
-export const BackButton = ({ onClick }) => (
-  <button onClick={onClick}>Back button</button>
-);
-
-
--- src/components/RecipePage/DishSummary.tsx
+++
@@ -1,9 +0,0 @@
-export const DishSummary = ({ recipeData }) => (
-  <div className="dish-summary-container">
-    <h2>{recipeData.title}</h2>
-    <p>Ready in {recipeData.readyInMinutes} minutes</p>
-    <p>Serves {recipeData.servings}</p>
-  </div>
-);
-
-
