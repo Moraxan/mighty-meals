13a75a61b21fe289508d288d50f4cedd8d3b8d8b
Chab93 <vikcrille93@gmail.com>
2023-04-18 09:42:57 +0200

    Added free text searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,12 @@ import Footer from './components/Footer/Footer';
 import TmpCard from './components/TmpCard/TmpCard';
 import TmpCardMTVMH from './components/TmpCard/TmpCardMTVMH';
 import SearchBar from './components/SearchBar/SearchBar';
+import SearchBarFreeText from './components/SearchBar/SearchBarFreeText';
 import SearchSwitch from './components/SearchSwitch/SearchSwitch';
 import {RecipeFrontST} from './components/Interface/Interface';
 import {RecipeMTVMH} from './components/Interface/Interface';
 import {useMediaQuery} from "./components/DropdownNav/DropdownNav";
+
 
 export default function App() {
   // Below 2 arrays are used to make it clear for TypeScript what types our useState functions require.
@@ -27,6 +29,7 @@ export default function App() {
   const [show, setShow] = useState(false);
 
   // States for all different type of filter/ingredient choices
+  const [freeTextSearch, setFreeTextSearch] = useState("");
   const [ingredientChoices, setIngredientChoices] = useState(emptyArr);
   const [mealChoice, setMealChoice] = useState("");
   const [cuisineChoices, setCuisineChoices] = useState(emptyArr);
@@ -70,7 +73,54 @@ export default function App() {
     createCards(forTesting);
   }, [])
 
+  async function getApiData(){
+    // Function that fetches / GET data back from the API.
+    // 2 endpoints which are controlled by state prop standardSearch. If true standard search will run, if false "man tager vad man haver" search will run.
 
+    // Settings, read spoonacular documentation for more info.
+    const apiKey: string = "6d398aefc8b6440286cd4509f45075c5";
+    const maxHits: number = 3;
+    const addRecipeNutrition: boolean = false;
+
+    // Settings only for MTVMH
+    const ranking: number = 1;    //Whether to maximize used ingredients (1) or minimize missing ingredients (2) first.
+    const ignorePantry: boolean = true;   //Whether to ignore typical pantry items, such as water, salt, flour, etc.
+
+    if(standardSearch === true){
+
+      const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&type=${mealChoice}&cuisine=${createURIString(cuisineChoices)}&includeIngredients=${createURIString(ingredientChoices)}&intolerance=${createURIString(intoleranceChoices)}&diet=${createURIString(dietChoices)}&query=${freeTextSearch}&number=${maxHits}&addRecipeInformation=true&addRecipeNutrition=${addRecipeNutrition}`;
+      console.log(url);
+      try{
+        const response = await fetch(encodeURI(url));
+        const result = await response.json();
+
+        createCards(result.results)
+      }
+      catch (e){
+        console.log(e);
+      }
+    } else{
+      // This search requires at least 1 ingredient, if none are selected an alert will pop-up telling the user to select at least 1..
+      if(ingredientChoices.length > 0){
+
+        const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${createURIString(ingredientChoices)}&ranking=${ranking}&ignorePantry=${ignorePantry}&number=${maxHits}`;
+
+        try{
+          const response = await fetch(encodeURI(url));
+          const result = await response.json();
+  
+          console.log(result);
+  
+          createCards(result)
+        }
+        catch (e){
+          console.log(e);
+        }
+      } else{
+        alert("Please select at least 1 ingredient...")
+      }
+    }
+  }
 
 
   // Receives data from API or the sample data and creates objects fit for start-page cards.
@@ -119,8 +169,13 @@ export default function App() {
       {matches === true && <SearchSwitch standardSearch={standardSearch} setStandardSearch={setStandardSearch} setMealChoice={setMealChoice} setCuisineChoices={setCuisineChoices} setIntoleranceChoices={setIntoleranceChoices} setDietChoices={setDietChoices} setSelected={setSelected}/>}
       <div className="app-body d-flex flex-column">
         <NavigationBar standardSearch={standardSearch} setStandardSearch={setStandardSearch} setMealChoice={setMealChoice} setCuisineChoices={setCuisineChoices} setIntoleranceChoices={setIntoleranceChoices} setDietChoices={setDietChoices} setSelected={setSelected}/>
+        
+        {standardSearch ?
+        <SearchBarFreeText freeTextSearch={freeTextSearch} setFreeTextSearch={setFreeTextSearch} show={show} setShow={setShow} showRed={true} getApiData={getApiData}/> : 
         <SearchBar show={show} setShow={setShow} ingredientChoices={ingredientChoices} setIngredientChoices={setIngredientChoices} showRed={true}/>
-        <SideBar show={show} setShow={setShow} mealChoice={mealChoice} setMealChoice={setMealChoice} cuisineChoices={cuisineChoices} setCuisineChoices={setCuisineChoices} intoleranceChoices={intoleranceChoices} setIntoleranceChoices={setIntoleranceChoices} dietChoices={dietChoices} setDietChoices={setDietChoices} selected={selected} setSelected={setSelected} ingredientChoices={ingredientChoices} setIngredientChoices={setIngredientChoices} showRed={false} createCards={createCards} standardSearch={standardSearch} setStandardSearch={setStandardSearch}/>
+        }
+        
+        <SideBar show={show} setShow={setShow} mealChoice={mealChoice} setMealChoice={setMealChoice} cuisineChoices={cuisineChoices} setCuisineChoices={setCuisineChoices} intoleranceChoices={intoleranceChoices} setIntoleranceChoices={setIntoleranceChoices} dietChoices={dietChoices} setDietChoices={setDietChoices} selected={selected} setSelected={setSelected} ingredientChoices={ingredientChoices} setIngredientChoices={setIngredientChoices} showRed={false} createCards={createCards} standardSearch={standardSearch} setStandardSearch={setStandardSearch} getApiData={getApiData}/>
         <br />
         <div className="d-flex flex-wrap justify-content-center align-self-center cardArea-styling">
         {recipesST.length > 0 && recipesST.map(recipe => <TmpCard key={recipe.id} recId={recipe.id} imgSrc={recipe.image} recipeTitle={recipe.title} readyInMin={recipe.readyInMinutes}/>)}
@@ -171,3 +226,26 @@ export default function App() {
         return "snack";
     }
   }
+
+  // Takes all choices from the source array and creates a string appropriate for the API URI.
+function createURIString(inputArray: string[]){
+  let returnString: string = "";
+
+  if(inputArray.length <= 0){
+      return "";
+  } else{
+      if(inputArray.length == 1){
+          return inputArray[0];
+      } else{
+          inputArray.forEach((item: string, index: number) => {
+              if(index + 1 < inputArray.length){
+                  returnString += `${item},`;
+              } else{
+                  returnString += item;
+              }
+          });
+
+          return returnString.toLowerCase();
+      }
+  }
+}
