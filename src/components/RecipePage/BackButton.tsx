76e3dc9efc446c1ad8e280690fa52acfe3d41d03
Daniel <daniel.langton@iths.se>
2023-04-20 16:32:56 +0200

    Changed some responsive css settings

--- src/components/RecipePage/BackButton.tsx
+++ src/components/RecipePage/BackButton.tsx
@@ -1,5 +1,9 @@
-export const BackButton = ({ onClick }) => (
-  <button onClick={onClick}>Back button</button>
-);
-
-
+import backbtn from "../../images/return-button.png";
+//@ts-ignore
+export const BackButton = ({ handleBackClick }) => {
+  return (
+    <div className="RecipePage-back-button" >      
+        <img className="back" alt="back-button"  src={backbtn} width="120" height="64" onClick={() => {handleBackClick();}}></img>
+    </div>
+  );
+};
