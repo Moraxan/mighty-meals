7e973a41a547633d37b2a117dc10a6f3a7d317fe
Carl-Johan Svensson <calletrumma@gmail.com>
2023-04-26 17:02:13 +0200

    Fixed an error that made the site not load.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Router } from './App'
-import {RouterProvider} from "react-router-dom";
+import {RouterProvider} fromq "react-router-dom";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
