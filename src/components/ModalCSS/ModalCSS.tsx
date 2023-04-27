ad20805a167ee265296f628fa492bdc3775872fb
Moraxan <83705522+Moraxan@users.noreply.github.com>
2023-04-27 13:52:09 +0200

    Fixed typos and things

--- src/components/ModalCSS/ModalCSS.tsx
+++ src/components/ModalCSS/ModalCSS.tsx
@@ -9,7 +9,7 @@ import "./ModalCSS.css";
 export default function ModalCSS(props) {
   const [show, setShow] = useState(true);
   const handleClose = () => setShow(false);
-  
+
   return (
     <>
       <Modal className="ModalSettings" show={show} onHide={handleClose} onExit={() => {props.setShowCSS(false)}}>
@@ -18,11 +18,11 @@ export default function ModalCSS(props) {
         </Modal.Header>
         <Modal.Body>
           <p>If we want CSS properties to be global we can set them as variables in index.css.<br></br>
-              We can then import them with import './index.css' (file path will depend on where your component is);<br></br>
+              We can then import it into your css file them with @import url('../../index.css'); (file path will depend on where your component is);<br></br>
               We should also use relative units (em, rem, %) instead of absolute units (px, pt, cm, in) for font sizes, margins, paddings, etc.<br></br>
               This is not the case at them moment and needs a double check.<br></br>
               <br></br>
-              Check the current index.css for global variable before you start a new component, please.<br></br>
+              Check the current index.css for global variables before you start a new component, please.<br></br>
           </p>
         </Modal.Body>
         <Modal.Footer>
