f575590bc6c7714bd99199a7f541e8d6ca548915
Moraxan <83705522+Moraxan@users.noreply.github.com>
2023-05-10 10:16:02 +0200

    Profile up with route also cleaned the router

---
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,12 @@
+import './ProfilePage.css';
+
+export const ProfilePage = () => {
+
+    return (
+        <div>
+            <h1>
+            Profile
+            </h1>
+        </div>
+    );
+}
