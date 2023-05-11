d2349f40ae3e3cae3d52264ab20b8669c7937bbf
Moraxan <83705522+Moraxan@users.noreply.github.com>
2023-05-11 14:43:13 +0200

    Cosmetic changes

--- src/components/Comment/CommentForm.tsx
+++ src/components/Comment/CommentForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
-import './Comment.css';
+import './CommentForm.css';
 
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
