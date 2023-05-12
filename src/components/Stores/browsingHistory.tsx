8303cf2c8d590914015434669fbd94094223aaee
Chab93 <vikcrille93@gmail.com>
2023-05-12 09:48:12 +0200

    backButton is now remembering

---
+++ src/components/Stores/browsingHistory.tsx
@@ -0,0 +1,6 @@
+import { create } from 'zustand'
+
+export const useBroserHistoryStore = create((set) => ({
+    previousPage: null,
+    setPreviousPage: (input: string) => set({previousPage: input}),
+}))
