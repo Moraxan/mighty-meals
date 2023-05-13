e1c5ee321d873733f2e533236075a5d71de07aee
Klebert-01 <arvid.berkert@gmail.com>
2023-05-13 11:22:06 +0200

    added conversion between table/teaspoon + bugfix

--- src/pages/RecipePage/Ingredients.tsx
+++ src/pages/RecipePage/Ingredients.tsx
@@ -6,15 +6,27 @@ import increaseBtn from "../../images/increaseBtn.png";
 function AdjustMeasurment(amount: any, unit: string) {
 
   if (unit === "ml") {
-    amount = amount >= 1000 ? (unit = "l", amount / 1000) : (amount >= 100 && amount < 1000) ? (unit = "dl", amount / 100) : amount;
+    amount = amount >= 1000 ? (unit = "l", amount / 1000) : (amount >= 50 && amount < 1000) ? (unit = "dl", amount / 100) : amount;
   }
   
   if (unit === "g") {
     amount = amount >= 1000 ? (unit = "kg", amount / 1000) : (amount >= 100 && amount < 1000) ? (unit = "hg", amount / 100) : amount;
   }
-  amount = amount < 0.1 ? 0.1 : amount;
 
-  //if first decimal of amount = 0, round down to integer (quick fix to solve bug) else shorten to display 1 decimal point
+  if (unit.toLowerCase() === "tbsps" || unit.toLowerCase() === "tbsp"){ //kanske inte behöver använda to lowercase här
+    unit = 'tbsp';
+    amount = amount < 1 ? (unit = "tsp", amount * 3) : amount;
+  }
+  
+  if (unit.toLowerCase() === "tsp" || unit.toLowerCase() === "tsps"){
+    unit = "tsp";
+    amount = amount >= 10 ? (unit = "tbsp", amount / 3) : amount;
+  }
+
+  //fix to avoid 0 displayed as amount if below 0.1, might not need after measurment conversions done
+  // amount = amount < 0.1 ? 0.1 : amount;
+
+  //if first decimal of amount = 0, round down to integer else shorten to display only 1 decimal point (quick fix to solve bug)
   amount = Number(amount.toFixed(1).split(".")[1][0]) === 0 ? Math.round(amount) : amount.toFixed(1);
 
   return { adjustedAmount: amount, adjustedUnit: unit };
@@ -60,6 +72,7 @@ export const Ingredients = ({ ingredients, noOfServings }) => {
       <div className="ingredients-list">
         <ul>
           {ingredientAmounts.map((amount: number, index: number) => {
+          
             const calculatedAmount = (amount * servings) / noOfServings;
 
             const { adjustedAmount, adjustedUnit } = AdjustMeasurment(calculatedAmount,ingredientUnits[index]);
@@ -68,9 +81,9 @@ export const Ingredients = ({ ingredients, noOfServings }) => {
               <li key={index}>
                 <strong>
                   {adjustedAmount}
-                </strong>
                   {" "}
                   {adjustedUnit}
+                </strong>
                 {" "}
                 {ingredientNames[index]}
               </li>
