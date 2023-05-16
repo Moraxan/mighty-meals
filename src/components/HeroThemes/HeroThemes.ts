f6bba8e91056bf573fac9d0df82f2ecf321cba42
Klebert-01 <arvid.berkert@gmail.com>
2023-05-16 19:52:48 +0200

    themes in place for all heroes + their recipe-page

--- src/components/HeroThemes/HeroThemes.ts
+++ src/components/HeroThemes/HeroThemes.ts
@@ -1,5 +1,6 @@
 
 export function SetHulkTheme(){
+
     var r = document.querySelector(':root');
     
     const lightGreen = '#9bc063';
@@ -21,8 +22,49 @@ export function SetHulkTheme(){
 
 export function SetThorTheme(){
 
+    var r = document.querySelector(':root');
+
+    const blueGray = '#e2eeed';
+    const gray = '#dadada';
+    const red = '#b80000';
+    const darkRed = '#5f0e0e';
+    const darkBrown = '#363131';
+
+
+
+        //@ts-ignore
+        r!.style.setProperty('--accentcolourpurpleNonStatic', darkBrown);
+        //@ts-ignore
+        r!.style.setProperty('--accentcolouryellowNonStatic', red );
+        //@ts-ignore
+        r!.style.setProperty('--accentcolourredNonStatic', darkBrown);
+        //@ts-ignore
+        r!.style.setProperty('--backgroundcolor', gray);
+        //@ts-ignore
+        r!.style.setProperty('--componentbackgroundNonStatic', darkRed);
+
 }
 
 export function SetCaptainAmericaTheme(){
+
+    var r = document.querySelector(':root');
+
+
+    const blue = '#0000ff';
+    const darkBlue = '#002d5c';
+    const red = '#870000';
+    const darkGray = '#a5a5ab';
+    const lightGray = '#f0f0f7';
+
+        //@ts-ignore
+        r!.style.setProperty('--accentcolourpurpleNonStatic', red);
+        //@ts-ignore
+        r!.style.setProperty('--accentcolouryellowNonStatic', blue );
+        //@ts-ignore
+        r!.style.setProperty('--accentcolourredNonStatic', red);
+        //@ts-ignore
+        r!.style.setProperty('--backgroundcolor', darkBlue);
+        //@ts-ignore
+        r!.style.setProperty('--componentbackgroundNonStatic', darkGray);
 
 }
